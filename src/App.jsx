db9a316125a81e64fc66e99a19e1f2d778f92d79
Nichollas Garcia <nichollascavalcanti@hotmail.co.uk>
2025-06-03 20:07:25 +0100

    some updates on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,54 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Food from "./Food.jsx";
-import Card from "./Card.jsx";
-import Button from "./Button/Button.jsx";
-import Student from "./Studentx.jsx";
-import UserGreeting from "./UserGreeting.jsx";
-import List from "./List.jsx";
-import ProfilePicture from "./ProfilePicture.jsx";
-import MyComponnet from "./MyComponnet.jsx";
-import Counter from "./Counter.jsx";
-import MyChangeComponent from "./MyChangeComponent.jsx";
-import ColorPicker from "./ColorPicker.jsx";
-import Objects from "./Objects.jsx";
-import Foods from "./Foods.jsx";
-import Cars from "./Cars.jsx";
-import ToDoList from "./ToDoList.jsx";
-import SecondComponent from "./ComponnentUseEffect.jsx";
-import Clock from "./Clock.jsx";
-import UseContext from "./UseContext.jsx";
-import UseRef from "./UseRef.jsx";
-import Stopwatch from "./Stopwatch.jsx";
+import Navbar from "./components/Navbar";
+import "./App.css";
 function App() {
   return (
     <>
-      <Stopwatch />
-      {/*
-      <Clock />
-      <UseRef />
-      <UseContext />
-      <Header />
-      <SecondComponent />
-      <ToDoList />
-      <Cars />
-      <Foods />
-      <Objects />
-      <Counter />
-      <ColorPicker />
-      <MyChangeComponent />
-      <MyComponnet />
-      <ProfilePicture />
-      <List />
-      <UserGreeting isLoggedIn={true} username="Sheila" />
-      <Student name="Nichollas" age={25} isStudent={false} />
-      <Student name="Lucas" age={23} isStudent={false} />
-      <Student name="Clarice" age={37} isStudent={true} />
-      <Card />
-      <Food />
-      <Footer />
-      */}
+      <div className="body-container">
+        <nav>
+          <Navbar />
+        </nav>
+        <main>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+          libero itaque quibusdam veritatis animi hic laudantium tempore
+          assumenda consequuntur numquam amet necessitatibus fugiat atque harum,
+          quae nobis illum aliquid officiis. Minima ipsam distinctio sequi
+          impedit dolorem quam dignissimos consequuntur ad magni illo tenetur
+          consectetur deserunt, fuga harum repellendus facere, quae, illum
+          doloribus repellat assumenda. Sequi dignissimos laudantium ullam
+          labore blanditiis? Animi asperiores dignissimos veritatis dolores
+          cumque facilis nihil rerum optio, ex corporis temporibus cum
+          consequuntur, repellendus explicabo! Et perspiciatis, nihil dicta
+          ipsam mollitia, nobis eum voluptatibus deleniti, perferendis totam
+          earum? Corrupti beatae, sequi voluptas esse velit possimus atque harum
+          facilis amet fuga ea quas consequuntur dicta repudiandae placeat,
+          ipsum doloremque nam incidunt nostrum nemo fugiat consectetur ex
+          porro? Recusandae, quam. Voluptate tempora quidem quia nihil quis!
+          Officiis illum tempore possimus, consequuntur temporibus adipisci
+          nobis neque ipsam consectetur, fuga, quod in fugit quidem molestias.
+          Aperiam quam quos vel placeat perspiciatis minima. Adipisci dolor
+          delectus blanditiis, ad earum quia incidunt nesciunt perspiciatis
+          minima assumenda in voluptates labore tempora magnam aliquid magni
+          inventore distinctio quasi architecto illum animi sit doloremque velit
+          iure. Omnis! Repellat enim eaque commodi cum molestias! Rem repellat
+          autem, consectetur cum quaerat dolor, expedita quia maxime nostrum
+          impedit minima sed quo. Maiores nihil commodi reiciendis veritatis?
+          Corporis vitae velit perspiciatis? Fugiat vitae, a nisi earum
+          laboriosam dignissimos dolorem, tenetur itaque atque consectetur
+          numquam officia porro aliquam magni ipsa eaque rem dicta, quaerat
+          tempora inventore? Eveniet accusantium amet ut minus repellendus! Sint
+          assumenda qui ducimus itaque laudantium adipisci? Voluptatibus
+          quisquam consectetur ex dicta ad? Iusto, repellat. Fugiat ipsam
+          eligendi, vitae facere accusamus quam debitis quis, officiis animi
+          recusandae similique maxime quaerat. Tempora illum, voluptatibus
+          quibusdam eveniet corrupti sit quia asperiores voluptate vitae eum
+          blanditiis rerum quos pariatur similique. Culpa harum qui totam
+          perferendis magni. Sapiente a, ipsam quae ut quia quos?
+        </main>
+        <aside>
+          <h2>Options</h2>
+        </aside>
+        <footer>Driving na Terra da Rainha</footer>
+      </div>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Profile</li>
+      </ul>
+    </>
+  );
+}
+
+export default Navbar;
